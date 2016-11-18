f500db2810e9c4c292d39e064b33765218f5d9d8
Igor Ranieri Elland <igor@elland.me>
2016-11-18 12:13:07 +0100

    Add Carthage support.

---
+++ HKDFKit/HKDFKit-iOS/HKDFKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  HKDFKit-iOS.h
+//  HKDFKit-iOS
+//
+//  Created by Igor Ranieri on 18/11/2016.
+//  Copyright © 2016 Frederic Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKDFKit-iOS.
+FOUNDATION_EXPORT double HKDFKit_iOSVersionNumber;
+
+//! Project version string for HKDFKit-iOS.
+FOUNDATION_EXPORT const unsigned char HKDFKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKDFKit_iOS/PublicHeader.h>
+
+#import <HKDFKit_iOS/HKDFKit.h>
